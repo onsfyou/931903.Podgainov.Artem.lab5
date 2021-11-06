1d82afe1e202c0bc3353a7efcb27dba7cbeef651
onsfyou <81429417+onsfyou@users.noreply.github.com>
2021-11-06 16:27:28 +0700

    lab5

---
+++ LAb5.js
@@ -0,0 +1,26 @@
+function openn1(){
+    var n1 = document.getElementById('n1');
+    n1.style.display = "block";
+}
+function openn2(){
+    var n2 = document.getElementById('n2');
+    n2.style.display = "block";
+}
+function openn3(){
+    var n3 = document.getElementById('n3');
+    n3.style.display = "block";
+}
+
+function closen1(){
+    var n1 = document.getElementById('n1');
+    n1.style.display = "none";
+}
+function closen2(){
+    var n2 = document.getElementById('n2');
+    n2.style.display = "none";
+}
+function closen3(){
+    var n3 = document.getElementById('n3');
+    n3.style.display = "none";
+}
+
